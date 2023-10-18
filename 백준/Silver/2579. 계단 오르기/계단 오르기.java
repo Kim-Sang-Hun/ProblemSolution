86dd54fedd99cd111d36053fa5d760e1b00ced4b
Kim-Sang-Hun <dev_sanghun@naver.com>
2023-10-18 20:25:11 +0900

    [Silver III] Title: 계단 오르기, Time: 232 ms, Memory: 17840 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,35 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-  public static void getDP(int[][] dp, List<Integer> score, int idx) {
-    if (idx >= dp.length) return;
-
-    dp[idx][0] = Math.max(dp[idx - 2][0], dp[idx - 2][1])  + score.get(idx);
-    dp[idx][1] = dp[idx - 1][0]  + score.get(idx);
-    getDP(dp, score, idx + 1);
-  }
-
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     int n = s.nextInt();
-    List<Integer> score = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-      score.add(s.nextInt());
+    int[] score = new int[n+1];
+    int[] dp = new int[n+1];
+    for (int i = 1; i < score.length; i++) {
+      score[i] = s.nextInt();
     }
-    if (n == 1) {
-      System.out.println(score.get(0));
-      return;
+    dp[1] = score[1];
+    if (n >= 2) {
+      dp[2] = score[1] + score[2];
     }
-    int[][] dp = new int[n][2];
-
-    dp[0][0] = score.get(0); dp[0][1] = score.get(0);
-    dp[1][0] = score.get(1); dp[1][1] = dp[0][0] + score.get(1);
-    getDP(dp, score, 2);
-
-    System.out.println(Math.max(dp[n-1][0], dp[n-1][1]));
+    for (int i = 3; i <= n; i++) {
+      dp[i] = Math.max(dp[i-2], dp[i-3] + score[i-1]) + score[i];
+    }
+    System.out.println(dp[n]);
   }
 
 }
