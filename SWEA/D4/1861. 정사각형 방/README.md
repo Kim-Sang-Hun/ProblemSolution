f3d6590b2ba54a5263a6a1cf3c15e56c13c84d35
KimSanghun <tpspfkdl@naver.com>
2024-02-07 16:07:53 +0900

    [D4] Title: 정사각형 방, Time: 296 ms, Memory: 37,760 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/README.md
@@ -0,0 +1,15 @@
+# [D4] 정사각형 방 - 1861 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LtJYKDzsDFAXc) 
+
+### 성능 요약
+
+메모리: 37,760 KB, 시간: 296 ms, 코드길이: 1,425 Bytes
+
+### 제출 일자
+
+2024-02-07 16:07
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution {
+
+	static int N, start, max;
+	static int[][] map;
+	static int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+	public static int dfs(int y, int x, int count) {
+		for (int i = 0; i < dirs.length; i++) {
+			int nY = y + dirs[i][0];
+			int nX = x + dirs[i][1];
+
+			if (nY >= N || nX >= N || nY < 0 || nX < 0 || map[nY][nX] - map[y][x] != 1) continue;
+			return dfs(nY, nX, count + 1);
+		}
+		return count;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			map = new int[N][];
+			max = 0;
+			start = Integer.MAX_VALUE;
+			for (int i = 0; i < N; i++) {
+				map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			}
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					int tmp = dfs(i, j, 1);
+					if (max == tmp) {
+						max = tmp;
+						start = Math.min(start, map[i][j]);
+					} else if (max < tmp) {
+						max = tmp;
+						start = map[i][j];
+					}
+				}
+			}
+			sb.append("#" + tc + " " + start + " " + max + System.lineSeparator());
+		}
+		System.out.print(sb);
+	}
+}
