09dab62ed69e30ca69e25186665b3227c31a805e
KimSanghun <tpspfkdl@naver.com>
2024-02-15 14:09:27 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 2,064 ms, Memory: 20,652 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,44 +1,59 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution {
-    public static int answer, N, start[], end[], arr[][];
+public class Solution {
+    public static int answer, N, start[], end[], order[], arr[][];
+    public static boolean[] visited;
     
-    public static void checkLen(boolean[] visited, int idx, int count, int len) {
-        if (len >= answer) return;
+    public static void checkLen(int count) {
         if (count == N) {
-        	answer = Math.min(answer, len + Math.abs(arr[idx][0] - end[0]) + Math.abs(arr[idx][1] - end[1]));
+        	int Y = start[0];
+        	int X = start[1];
+        	int sum = 0;
+        	for (int i = 0; i < N; i++) {
+        		int nY = arr[order[i]][0];
+        		int nX = arr[order[i]][1];
+				sum += Math.abs(Y - nY) + Math.abs(X - nX);
+				Y = nY;
+				X = nX;
+			}
+        	sum += Math.abs(Y - end[0]) + Math.abs(X - end[1]);
+        	answer = Math.min(answer, sum);
             return;
         }
         for (int i = 0; i < N; i++) {
             if (visited[i]) continue;
             visited[i] = true;
-        	checkLen(visited, i, count + 1, len + Math.abs(arr[idx][0] - arr[i][0]) + Math.abs(arr[idx][1] - arr[i][1]));
+            order[count] = i;
+        	checkLen(count + 1);
             visited[i] = false;
         }
         
     }
     
-	public static void main(String[] args) {
-    	Scanner s = new Scanner(System.in);
-        int T = s.nextInt();
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         
         for (int tc = 1; tc <= T; tc++) {
-        	N = s.nextInt();
+        	N = Integer.parseInt(br.readLine());
             answer = Integer.MAX_VALUE;
-            start = new int[] {s.nextInt(), s.nextInt()};
-            end = new int[] {s.nextInt(), s.nextInt()};
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            start = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+            end = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
             arr = new int[N][2];
             for (int i = 0; i < N; i++) {
-            	arr[i][0] = s.nextInt();
-                arr[i][1] = s.nextInt();
+            	arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
             }
-            for (int i = 0; i < N; i++) {
-            	boolean[] visited = new boolean[N];
-                visited[i] = true;
-                checkLen(visited, i, 1, Math.abs(start[0] - arr[i][0]) + Math.abs(start[1] - arr[i][1]));
-            }
-        	System.out.printf("#%d %d\n", tc, answer);
+            visited = new boolean[N];
+            order = new int[N];
+            checkLen(0);
+        	sb.append("#" + tc + " " + answer + System.lineSeparator());
         }
-        
+        System.out.print(sb);
     }
 }
