bc60984cdef5300576715efa393ba1d96c7e6b9e
KimSanghun <tpspfkdl@naver.com>
2024-02-15 14:16:31 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 312 ms, Memory: 20,488 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -4,30 +4,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static int answer, N, start[], end[], order[], arr[][];
+    public static int answer, N, home[], arr[][];
+    public static int Y, X;
     public static boolean[] visited;
     
-    public static void checkLen(int count) {
+    public static void checkLen(int count, int len) {
+    	if (answer <= len) return;
         if (count == N) {
-        	int Y = start[0];
-        	int X = start[1];
-        	int sum = 0;
-        	for (int i = 0; i < N; i++) {
-        		int nY = arr[order[i]][0];
-        		int nX = arr[order[i]][1];
-				sum += Math.abs(Y - nY) + Math.abs(X - nX);
-				Y = nY;
-				X = nX;
-			}
-        	sum += Math.abs(Y - end[0]) + Math.abs(X - end[1]);
-        	answer = Math.min(answer, sum);
+        	len += Math.abs(Y - home[0]) + Math.abs(X - home[1]);
+        	answer = Math.min(answer, len);
             return;
         }
         for (int i = 0; i < N; i++) {
             if (visited[i]) continue;
             visited[i] = true;
-            order[count] = i;
-        	checkLen(count + 1);
+            int tmpY = Y;
+            int tmpX = X;
+            Y = arr[i][0];
+            X = arr[i][1];
+        	checkLen(count + 1, len + Math.abs(tmpY - arr[i][0]) + Math.abs(tmpX - arr[i][1]));
+        	Y = tmpY;
+        	X = tmpX;
             visited[i] = false;
         }
         
@@ -42,16 +39,16 @@ public class Solution {
         	N = Integer.parseInt(br.readLine());
             answer = Integer.MAX_VALUE;
             StringTokenizer st = new StringTokenizer(br.readLine());
-            start = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
-            end = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+            Y = Integer.parseInt(st.nextToken());
+            X = Integer.parseInt(st.nextToken());
+            home = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
             arr = new int[N][2];
             for (int i = 0; i < N; i++) {
             	arr[i][0] = Integer.parseInt(st.nextToken());
                 arr[i][1] = Integer.parseInt(st.nextToken());
             }
             visited = new boolean[N];
-            order = new int[N];
-            checkLen(0);
+            checkLen(0, 0);
         	sb.append("#" + tc + " " + answer + System.lineSeparator());
         }
         System.out.print(sb);
