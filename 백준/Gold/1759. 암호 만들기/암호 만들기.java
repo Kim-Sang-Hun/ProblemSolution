f3013e210006cd0f2435950b07b3fe3058b04ac8
KimSanghun <tpspfkdl@naver.com>
2024-02-21 16:27:19 +0900

    [Gold V] Title: 암호 만들기, Time: 132 ms, Memory: 15108 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,53 +1,56 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int L, C;
-    static List<String> list;
+
+	static int L, C;
+    static char[] list;
+    static char[] code;
     static PriorityQueue<String> queue = new PriorityQueue<>();
 
-    public static void bfs(int idx, String str) {
-        if (idx >= L) {
+    public static void combi(int idx, int start) {
+        if (idx == L) {
+            int count = 0;
+            for (int i = 0; i < L; i++) {
+                char c = code[i];
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'u' || c == 'o') ++count;
+            }
+            if (count != 0 && L - count >= 2) {
+            	StringBuilder sb = new StringBuilder();
+            	for (int i = 0; i < code.length; i++) {
+					sb.append(code[i]);
+				}
+            	queue.add(sb.toString());
+            }
             return;
         }
-        if (idx == L - 1) {
-            int count = 0;
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'u' || c == 'o') {
-                    count++;
-                }
-            }
-            if (count == 0) {
-                return;
-            } else if (L - count < 2) {
-                return;
-            } else {
-                queue.add(str);
-            }
-        }
-        for (int i = idx; i < C; i++) {
-            String s = list.get(i);
-            if (str.charAt(idx) < s.charAt(0)) {
-                bfs(idx + 1, str + s);
-            }
-        }
+        for (int i = start; i < C; i++) {
+			code[idx] = list[i];
+			combi(idx + 1, i + 1);
+		}
     }
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        L = s.nextInt();
-        C = s.nextInt();
-        list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+        list = new char[C];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < C; i++) {
-            String a = s.next();
-            list.add(a);
+            list[i] = st.nextToken().charAt(0);
         }
-        Collections.sort(list);
-        for (int i = 0; i < C - L + 1; i++) {
-            bfs(0, list.get(i));
-        }
+        code = new char[L];
+        Arrays.sort(list);
+        combi(0, 0);
         for (String str: queue) {
-            System.out.println(str);
+            sb.append(str).append(System.lineSeparator());
         }
+        System.out.println(sb);
     }
 }
