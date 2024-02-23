0e0a451c9de216219bd92501280e50a39cdc87ff
KimSanghun <tpspfkdl@naver.com>
2024-02-23 15:07:56 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 897 ms, Memory: 75,860 KB -BaekjoonHub

---
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int N;
+	static double minTotalLevy;
+	static boolean[] visited;
+	static Island[] islands;
+	static double E;
+	
+	static class Island {
+		int y, x;
+
+		public Island(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+	}
+	
+	static class Edge implements Comparable<Edge> {
+		int to;
+		double weight;
+
+		public Edge(int to, double weight) {
+			this.to = to;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Double.compare(this.weight, o.weight);
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			islands = new Island[N];
+			visited = new boolean[N];
+			minTotalLevy = 0;
+
+			for (int i = 0; i < N; i++) {
+				islands[i] = new Island(0, 0);
+			}
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				islands[i].x = Integer.parseInt(st.nextToken());
+			}
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				islands[i].y = Integer.parseInt(st.nextToken());
+			}
+			E = Double.parseDouble(br.readLine());
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			pq.add(new Edge(0, 0));
+			int count = 0;
+			
+			while (!pq.isEmpty()) {
+				Edge cur = pq.poll();
+				
+				if (visited[cur.to]) continue;
+				if (++count == N + 1) break;
+				Island curIsland = islands[cur.to];
+				visited[cur.to] = true;
+				minTotalLevy += cur.weight;
+				for (int i = 0; i < N; i++) {
+					if (visited[i]) continue;
+					Island nextIsland = islands[i];
+					pq.add(new Edge(i, getWeight(curIsland.y, curIsland.x, nextIsland.y, nextIsland.x)));
+				}
+				
+			}
+			
+			sb.append("#" + tc + " " + Math.round(minTotalLevy) + System.lineSeparator());
+		}
+		System.out.println(sb);
+	}
+	
+	public static double getWeight(int y, int x, int nY, int nX) {
+		return (Math.pow(x - nX, 2) + Math.pow(y - nY, 2)) * E;
+	}
+
+}
