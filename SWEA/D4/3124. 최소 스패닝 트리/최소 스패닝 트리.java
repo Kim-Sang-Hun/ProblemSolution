aa9e1fea5beeee674531b85dd95bd70c4e47b15b
KimSanghun <tpspfkdl@naver.com>
2024-02-23 14:03:37 +0900

    [D4] Title: 최소 스패닝 트리, Time: 4,423 ms, Memory: 199,132 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,84 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int[] parents;
-	static int T, V, E;
-	static Edge[] edges;
-	
 	static class Edge implements Comparable<Edge> {
-		int from;
-		int to;
-		int weight;
-		public Edge(int from, int to, int weight) {
-			this.from = from;
+		int to, weight;
+
+		public Edge(int to, int weight) {
 			this.to = to;
 			this.weight = weight;
 		}
+
 		@Override
 		public int compareTo(Edge o) {
 			return Integer.compare(this.weight, o.weight);
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		T = Integer.parseInt(st.nextToken());
-		
+		int T = Integer.parseInt(st.nextToken());
+
 		for (int tc = 0; tc < T; tc++) {
 			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-			edges = new Edge[E];
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			List<Edge>[] map = new ArrayList[V];
+			boolean[] visited = new boolean[V];
+			
+			for (int i = 0; i < V; i++) {
+				map[i] = new ArrayList<>();
+			}
 
 			for (int i = 0; i < E; i++) {
 				st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				int weight = Integer.parseInt(st.nextToken());
-				edges[i] = new Edge(from, to, weight);
+				map[from - 1].add(new Edge(to - 1, weight));
+				map[to - 1].add(new Edge(from - 1, weight));
 			}
-			Arrays.sort(edges);
-			makeSet();
-			long weight = 0;
-			int count = 0;
-			for (Edge edge : edges) {
-				if (!union(edge.from, edge.to)) continue;
-				weight += edge.weight;
-				if (++count == V - 1) break;
+
+			long result = 0;
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			pq.offer(new Edge(0, 0));
+
+			while (!pq.isEmpty()) {
+				Edge cur = pq.poll();
+				if (!visited[cur.to]) {
+					visited[cur.to] = true;
+					result += cur.weight;
+					for (Edge next : map[cur.to]) {
+						pq.add(next);
+					}
+				}
 			}
-			sb.append("#" + (tc + 1) + " " + weight + System.lineSeparator());
+
+			sb.append("#" + (tc + 1) + " " + result + System.lineSeparator());
 		}
 		System.out.println(sb);
-	}
-
-	public static void makeSet() {
-		parents = new int[V + 1];
-		
-		for (int i = 1; i < parents.length; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	public static int find(int a) {
-		if (parents[a] == a) return a;
-		
-		return parents[a] = find(parents[a]);
-	}
-	
-	public static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if (aRoot == bRoot) return false;
-		
-		parents[bRoot] = aRoot;
-		return true;
 	}
 }
