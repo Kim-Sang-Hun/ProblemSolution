864e24ce500444ede593f32624b9c1c43611cfa3
KimSanghun <tpspfkdl@naver.com>
2024-02-29 12:07:08 +0900

    [Gold V] Title: 동전 1, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -30,29 +29,3 @@ public class Main {
 		System.out.println(dp[K]);
 	}
 }
-
-
-/*
- * 10
- * 1
- * 2
- * 5
- * 1 1 - 1
- * 2 11 2 - 2
- * 3 111 12 - 2
- * 4 1111 112 22 - 3
- * 5 11111 1112 122 5 - 4
- * 6 111111 11112 1122 222 15 - 5
- * 
- * 10
- * 2
- * 3
- * 5
- * 1 0
- * 2 2 - 1
- * 3 3 - 1
- * 4 0
- * 5 23 5 - 2
- * 6 222 33 - 2
- * 
- */
