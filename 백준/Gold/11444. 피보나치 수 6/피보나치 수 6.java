3e03696c7de4da4154b1b8de24fd174e0d748e47
KimSanghun <tpspfkdl@naver.com>
2024-05-07 19:29:15 +0900

    [Gold II] Title: 피보나치 수 6, Time: 208 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+ * 도가뉴 항등식 : f(m+n) = f(m-1)f(n) + f(m)f(n+1)
+ * (짝수항) m=n, f(2n) = f(n) * (f(n) + 2 * f(n-1))
+ * (홀수항) m=n+1, f(2n+1) = f(n)^2 + f(n+1)^2
+ */
+public class Main {
+
+   static Map<Long, Long> fibo = new HashMap<>();
+   static int dividend = 1_000_000_007;
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      long n = sc.nextLong();
+
+      fibo.put(0L, 0L);
+      fibo.put(1L, 1L);
+      fibo.put(2L, 1L);
+      fibo.put(3L, 2L);
+      System.out.println(getFibo(n));
+   }
+
+   private static long getFibo(long n) {
+      if (fibo.get(n) != null) return fibo.get(n);
+      if (n % 2 == 0) {
+         long fb = ((getFibo(n / 2) % dividend) * (getFibo(n/2) % dividend + 2 * getFibo(n/2-1) % dividend) % dividend) % dividend;
+         fibo.put(n, fb);
+         return fb;
+      } else {
+         long fb = ((getFibo(n / 2) % dividend) * (getFibo(n/2) % dividend) % dividend
+             + (getFibo(n/2 + 1) % dividend) * (getFibo(n/2 + 1) % dividend) % dividend) % dividend;
+         fibo.put(n, fb);
+         return fb;
+      }
+   }
+}
