355b32a7e000aa0282083de53e1bb8568c7c6ad7
KimSanghun <tpspfkdl@naver.com>
2024-06-21 16:51:40 +0900

    [Gold V] Title: 회장뽑기, Time: 132 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+   static boolean[] visited;
+   static int n, minPoint;
+   static List<Integer> candidates;
+   static List<List<Integer>> friends;
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      n = Integer.parseInt(br.readLine());
+      candidates = new LinkedList<>();
+      friends = new ArrayList<>();
+      minPoint = Integer.MAX_VALUE;
+      visited = new boolean[n + 1];
+      for (int i = 0; i <= n; i++) {
+         friends.add(new LinkedList<>());
+      }
+      while (true) {
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         int p1 = Integer.parseInt(st.nextToken());
+         int p2 = Integer.parseInt(st.nextToken());
+         if (p1 == -1 || p2 == -1) break;
+         friends.get(p1).add(p2);
+         friends.get(p2).add(p1);
+      }
+      for (int i = 1; i <= n; i++) {
+         getPoint(i);
+
+      }
+      System.out.println(minPoint + " " + candidates.size());
+      for (int i : candidates) {
+         System.out.print(i + " ");
+      }
+   }
+
+   private static void getPoint(int member) {
+      Queue<int[]> qu = new LinkedList<>();
+      qu.add(new int[] {member, 0});
+      visited = new boolean[n + 1];
+      visited[member] = true;
+      int max = 0;
+      while (!qu.isEmpty()) {
+         int[] cur = qu.poll();
+         max = cur[1];
+         if (max > minPoint) {
+            break;
+         }
+         for (int i : friends.get(cur[0])) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            qu.add(new int[] {i, cur[1] + 1});
+         }
+      }
+      if (max < minPoint) {
+         minPoint = max;
+         candidates = new LinkedList<>();
+      }
+      if (max == minPoint) {
+         candidates.add(member);
+      }
+   }
+}
